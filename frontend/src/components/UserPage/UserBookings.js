f1e13f4d527828f757b1d5faff460715ee0e5dcb
Justinguu <justinguuu@gmail.com>
2022-12-04 16:34:54 -0500

    boookings css

--- frontend/src/components/UserPage/UserBookings.js
+++ frontend/src/components/UserPage/UserBookings.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { getBookingsByUserthunk } from "../../store/bookings";
 import { deleteBookingId } from "../../store/bookings";
 import { getAllSpotsThunk } from "../../store/spots";
-import realEstate from "../icons/RealEstate.png"
+import realEstate from "../icons/RealEstate.png";
 import "./UserBookings.css";
 
 function UserBookings() {
@@ -15,13 +15,11 @@ function UserBookings() {
   const history = useHistory();
   const { spotId } = useParams();
 
-
   bookings.sort(function (a, b) {
     return new Date(a.endDate) - new Date(b.endDate);
   });
 
   const [isLoaded, setIsLoaded] = useState(false);
-
 
   const redirectUser = () => {
     history.push(`/spots/${spotId}`);
@@ -46,43 +44,37 @@ function UserBookings() {
   } else {
     userBookings = (
       <div className="profile-container">
-        <div className="cover-photo-container">
-        </div>
+        <div className="cover-photo-container"></div>
         <div className="user-title-container">
-        <h2 className="mySpotHeader">Upcoming Bookings</h2>
-          </div>
-          <div className="user-showcase-container">
+          <h2 className="mySpotHeader">Upcoming Bookings</h2>
+        </div>
+        <div className="user-showcase-container">
+            {bookings.map((booking) => (
           <div className="user-showcase-wrapper">
-          {bookings.map((booking) => (
-            <div className="user-singleImgContainer" key={booking.id}>
-             
-                  <h3 className="spotAddy-Owned spacing">{booking.Spot.name}</h3>
+              <div className="user-singleImgContainer" key={booking.id}>
+                <h3 className="spotAddy-Owned spacing">{booking.Spot.name}</h3>
                 <div className="booking-card-right" onClick={() => history.push(`/spots/${booking.Spot.id}`)}>
                   <img className="booking-image" src={booking.Spot.previewImage} alt="Spot" />
-                <div className="booking-card-buttons">
-                  {/* <NavLink to={`/spots/${booking.Spot.id}`} className="spacing booking-card-button">
+                  <div className="booking-card-buttons">
+                    {/* <NavLink to={`/spots/${booking.Spot.id}`} className="spacing booking-card-button">
                     View Spot
                   </NavLink> */}
-                  <button
-                    className="cancel-booking-bttn"
-                    onClick={() => dispatch(deleteBookingId(booking.id))}
-                  >
-                    Cancel Booking
-                  </button>
-                </div>
+                    <button className="cancel-booking-bttn" onClick={() => dispatch(deleteBookingId(booking.id))}>
+                      Cancel Booking
+                    </button>
+                  </div>
                 </div>
                 <div className="booking-card-info">
                   <p className="grouping-info spacing">
-                    {new Date(booking.startDate).toLocaleDateString()} -{" "}
-                    {new Date(booking.endDate).toLocaleDateString()}
-                    &nbsp;&nbsp;{booking.Spot.city}, {booking.Spot.state} &nbsp;
+                    <div className="booking-info-font"> Booking Information</div>{" "}
+                   <div className="booking-date">{new Date(booking.startDate).toLocaleDateString()} -{" "}{new Date(booking.endDate).toLocaleDateString()}</div> 
+                    <div>{booking.Spot.address},&nbsp;{booking.Spot.city} {booking.Spot.state}</div>
                   </p>
                 </div>
+              </div>
             </div>
-          ))}
+            ))}
           </div>
-        </div>
-        
       </div>
     );
   }
@@ -90,7 +82,7 @@ function UserBookings() {
   return (
     <div className="user-booking-container">
       <div className="user-booking-inner-container">
-        {/* <div className="user-booking-header mySpotHeader">My Bookings</div> */}
+  
         {userBookings}
       </div>
     </div>
