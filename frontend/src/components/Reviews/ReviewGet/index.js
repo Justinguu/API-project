a445a57e25cfcb6a5b4fc77a914a95b9163eba66
Justinguu <justinguuu@gmail.com>
2022-12-20 23:21:51 -0500

    fixed up review css

--- frontend/src/components/Reviews/ReviewGet/index.js
+++ frontend/src/components/Reviews/ReviewGet/index.js
@@ -5,8 +5,8 @@ import { Modal } from "../../../context/Modal";
 import { getCurrSpotThunk } from "../../../store/spots";
 import EditReview from "../EditModal/editReview";
 import editIcon from "../../icons/edit-icon.png";
-import trashIcon from "../../icons/trashIcon.png"
-import Bstar from "../../icons/BBStar.png"
+import trashIcon from "../../icons/trashIcon.png";
+import Bstar from "../../icons/BBStar.png";
 import icon from "../../Navigation/Images/icon.svg";
 
 import "./reviewGet.css";
@@ -43,46 +43,62 @@ const GetSpotReviews = ({ spotId }) => {
             return (
               <div className="review-box" key={review.id}>
                 <div className="reviewe-box" style={{ fontSize: "16px", fontWeight: "800px" }}>
-                  <div>
-                    <img className="review-icon" src={icon} alt="" />
-                  </div>{" "}
-                  <div className="review-name">&nbsp;&nbsp;{review.User?.firstName}</div>
+                  <div className="review-box-left">
+                    <div>
+                      <img className="review-icon" src={icon} alt="" />
+                    </div>
+
+                    <div className="reviewNameandDate">
+                      <div className="review-name">{review.User?.firstName}</div>
+                      <div className="review-date">
+                        Posted on: {review.createdAt.slice(5, 7)}/{review.createdAt.slice(8, 9)}/
+                        {review.createdAt.slice(0, 4)}
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="review-box-right">
+                  {!sessionUser
+                      ? null
+                      : sessionUser.id === review.userId && (
+                          <img
+                            className="editButtonRevieww"
+                            src={editIcon}
+                            onClick={() => {
+                              setShowUpdateReview(true);
+                              setCurrReview(review);
+                            }}
+                          ></img>
+                        )}
+                    {showUpdateReview && (
+                      <Modal onClose={() => setShowUpdateReview(false)}>
+                        <EditReview currReview={currReview} spotId={spotId} setShowUpdateReview={setShowUpdateReview} />
+                      </Modal>
+                    )}
+                    {!sessionUser
+                      ? null
+                      : sessionUser.id === review.userId && ( // even if not session user still be able to access spots
+                          <img
+                            className="deleteButtonRevieww"
+                            src={trashIcon}
+                            onClick={(e) => deleteReview(e, review.id)}
+                          ></img>
+                        )}
+                  </div>
                 </div>
+                <div className="review-body-container">
                   <div className="star-review-container">
-             
-                <table><tr><td>{review.stars}</td><td><img className="BBStar" src={Bstar}/></td></tr></table>
-               </div> 
-                <div className="review-text">
-                  {review.review} &nbsp; &nbsp;
-                  
-                  {!sessionUser
-                    ? null
-                    : sessionUser.id === review.userId && (
-                        <img
-                          className="editButtonRevieww"
-                          src={editIcon}
-                          onClick={() => {
-                            setShowUpdateReview(true);
-                            setCurrReview(review);
-                          }}
-                        ></img>
-                      )}
-                  {showUpdateReview && (
-                    <Modal onClose={() => setShowUpdateReview(false)}>
-                      <EditReview
-                        currReview={currReview}
-                        spotId={spotId}
-                        setShowUpdateReview={setShowUpdateReview}
-                      />
-                    </Modal>
-                  )}
-                  {!sessionUser
-                    ? null
-                    : sessionUser.id === review.userId && ( // even if not session user still be able to access spots
-                        <img className="deleteButtonRevieww" src={trashIcon} onClick={(e) => deleteReview(e, review.id)}>
-                        
-                        </img>
-                      )}
+                    <table>
+                      <tr>
+                        <td>
+                          <img className="BBStar" src={Bstar} />
+                        </td>
+                        {review.stars}
+                        <td></td>
+                      </tr>
+                    </table>
+                  </div>
+                  <div className="review-text">{review.review} &nbsp; &nbsp;</div>
                 </div>
               </div>
             );
