66f5da28b85c984189784b3bc9d1a42f8da26aa0
Justinguu <justinguuu@gmail.com>
2022-12-22 16:37:57 -0500

    update seeders

--- backend/db/seeders/20220802161341-images.js
+++ backend/db/seeders/20220802161341-images.js
@@ -115,7 +115,7 @@ module.exports = {
       userId: 15
      },
      {
-      url: 'https://images.unsplash.com/photo-1540882082344-a56ecd70ba96?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+      url: 'https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2835&q=80',
       previewImage: true,
       spotId: 16,
       reviewId: null,
